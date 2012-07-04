9d2ea25a29886ffbbfc8bf89910e72fc84dc2503
vinitlee <vinitlee@gmail.com>
2012-07-04 01:30:50 -0700

    Added keystroke support

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-preload = new Image();
-
 update = function() {
     num = parseInt($('#number').attr('value'))
     $('.comic').attr('src','http://questionablecontent.net/comics/'+num+'.png');
@@ -18,6 +16,28 @@ decrement = function() {
     comic.attr('value',(currentval<=1)?1:currentval-1);
     update();
 }
+paginate = function() {
+    segment = $('.comic').height()/4;
+    maxScroll = $('.comic').height()-$(window).height();
+    curScroll = $(document).scrollTop();
+    if (curScroll >= maxScroll) {
+        increment();
+        return null;
+    }
+    $(document).scrollTop(Math.floor((curScroll+1)/segment+1)*segment);
+}
+depaginate = function() {
+    segment = $('.comic').height()/4;
+    curScroll = $(document).scrollTop();
+    $(document).scrollTop(Math.floor((curScroll+1)/segment-1)*segment);
+}
+
+preload = new Image();
+
+$('body').ready(function() {
+    $('#number').attr('value',localStorage.number);
+    update();
+});
 $('.back').click(decrement);
 $('.forward').click(increment);
 $('#number').blur(update);
@@ -27,7 +47,33 @@ $('#number').keypress(function(e){
         update();
     }
 });
-$('body').ready(function() {
-    $('#number').attr('value',localStorage.number);
-    update();
+$('#number').keypress(function(e){
+    if (e.which==13) {
+        this.blur();
+        update();
+        return null;
+    }
+});
+
+$(document).keydown(function(e){
+    // Left
+    if (e.which == 37) { 
+      decrement();
+      return null;
+    }
+    // Right
+    if (e.which == 39) { 
+      increment();
+      return null;
+    }
+    // Down
+    if (e.which == 40) { 
+      paginate();
+      return null;
+    }
+    // Up
+    if (e.which == 38) { 
+      depaginate();
+      return null;
+    }
 });
